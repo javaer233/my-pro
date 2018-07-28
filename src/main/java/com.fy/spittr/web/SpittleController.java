3e0503987683bd133d9b7e883570a72698c4d5a6
carl <1074582384@qq.com>
2018-07-28 10:03:50 +0800

    spittle Test

--- src/main/java/com.fy/spittr/web/SpittleController.java
+++ src/main/java/com.fy/spittr/web/SpittleController.java
@@ -1,11 +1,15 @@
 package com.fy.spittr.web;
 
 import com.fy.spittr.data.SpittleRepository;
+import org.apache.tools.ant.taskdefs.Get;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 /**
  * Created by yk on 2018/07/25
@@ -14,15 +18,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/spittles")
 public class SpittleController {
     private SpittleRepository spittleRepository;
+
+    public SpittleController(){}
+
     @Autowired
     public SpittleController(SpittleRepository spittleRepository){
         this.spittleRepository = spittleRepository;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String spittles(Model model){
+    @RequestMapping(method = GET)
+    public String spittles(@Valid Model model){
         model.addAllAttributes(spittleRepository.findSpittles(Long.MAX_VALUE, 20));
         return "spittles";
+    }
+
+    @RequestMapping(value = "register", method = GET)
+    public String showRegistrationForm(){
+        return "registerForm";
     }
 
 }
--- src/main/java/com.fy/spittr/web/SpittleControllerTest.java
+++ src/main/java/com.fy/spittr/web/SpittleControllerTest.java
@@ -3,19 +3,42 @@ package com.fy.spittr.web;
 import com.fy.spittr.Spittle;
 import com.fy.spittr.data.SpittleRepository;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.view.InternalResourceView;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import static org.junit.matchers.JUnitMatchers.hasItems;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+//import org.mockito.Mockito;
 /**
  * Created by yk on 2018/07/25
  */
 public class SpittleControllerTest {
 
     @Test
-    public void shouldShowRecentSpittles(){
-        //SpittleRepository mockRepository = mock(SpittleRepository.class);
+    public void shouldShowRecentSpittles() throws Exception {
+        List<Spittle> expectedSpittles = createSpittleList(20);
+        SpittleRepository mockRepository = mock(SpittleRepository.class);
+        when(mockRepository.findSpittles(Long.MAX_VALUE, 20)).thenReturn(expectedSpittles);
+
+        SpittleController controller = new SpittleController(mockRepository);
+        MockMvc mockMvc = standaloneSetup(controller)
+                .setSingleView(new InternalResourceView("/WEB_INF/views/spittles.jsp")).build();
+
+        //对/spittles发起请求
+        //mockMvc.perform(get("/spittles"))
+        //        .andExpect(view().name("spittles"))
+        //        .andExpect(model().attributeExists("spittleList"))
+        //        .andExpect(model().attribute("spittleList", hasItems(expectedSpittles.toArray())));
     }
 
     private List<Spittle> createSpittleList(int count){
@@ -24,6 +47,15 @@ public class SpittleControllerTest {
             spittles.add(new Spittle("Spittle " + i, new Date()));
         }
         return spittles;
+    }
+
+    @Test
+    public void showRegistrationTest() throws Exception {
+        SpittleController spittleController = new SpittleController();
+        MockMvc build = standaloneSetup(spittleController).build();
+
+        build.perform(get("/spittles/register"))
+                .andExpect(view().name("registerForm"));
     }
 
 }
